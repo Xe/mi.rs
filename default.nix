b0c65d5b1d24bb5fbbc3d7125f70c489bc0560b6
Christine Dodrill <me@christine.website>
2020-11-17 14:05:59 -0500

    embed migrations, create migration tool, make nix build work

--- default.nix
+++ default.nix
@@ -1,30 +1,46 @@
 { sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
 with pkgs;
 let
-  backend = pkgs.callPackage ./backend { inherit sources pkgs; };
-  frontend = pkgs.callPackage ./sina { inherit sources pkgs; };
-  gruvbox = pkgs.callPackage sources.gruvbox-css { };
-in stdenv.mkDerivation {
-  pname = "mi";
-  version = "${backend.version}";
-  phases = "installPhase";
+  backend = callPackage ./backend { inherit sources pkgs; };
+  frontend = callPackage ./sina { inherit sources pkgs; };
+  gruvbox = callPackage sources.gruvbox-css { };
 
-  installPhase = ''
-    # service binary
-    mkdir -p $out/bin
+  composite = stdenv.mkDerivation {
+    pname = "mi";
+    version = "${backend.version}";
+    phases = "installPhase";
 
-    for file in ${backend}/bin/*; do
-      ln -s ${backend}/bin/$(${coreutils}/bin/basename $file) $out/bin/$(${coreutils}/bin/basename $file)
-    done
+    installPhase = ''
+      # service binary
+      mkdir -p $out/bin
 
-    # frontend JS
-    mkdir -p $out/public/js
-    ln -s ${frontend}/Main.js $out/public/js/elm.js
+      for file in ${backend}/bin/*; do
+        ln -s ${backend}/bin/$(basename $file) $out/bin/$(basename $file)
+      done
 
-    # static files
-    cp -vrf ${./static}/* $out/public
+      # static files
+      mkdir -p $out/public/
+      cp -vrf ${./static}/* $out/public
 
-    # migrations
-    ln -s ${./backend/migrations} $out/migrations
+      # frontend JS
+      rm $out/public/elm.js
+      ln -s ${frontend}/Main.min.js $out/public/elm.js
+    '';
+  };
+
+  wrapper = writeScriptBin "mi-backend" ''
+    #!${pkgs.stdenv.shell}
+    set -e
+    set -x
+
+    export RUST_LOG=info
+    export DATABASE_URL=./mi.db
+    export ROCKET_DATABASES='{ main_data = { url = "./mi.db" } }';
+    ${composite}/bin/migrate_database
+    export ROCKET_ASSET_PATH=${composite}/public
+    exec ${composite}/bin/mi
   '';
+in symlinkJoin {
+  name = "mi";
+  paths = [ wrapper composite ];
 }
