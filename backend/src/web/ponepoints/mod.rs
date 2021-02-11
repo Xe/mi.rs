24dfd73d578de044553d7d0180f1acc7e5b63d39
Christine Dodrill <me@christine.website>
2021-02-10 21:37:59 -0500

    fairing
    Signed-off-by: Christine Dodrill <me@christine.website>

--- backend/src/web/ponepoints/mod.rs
+++ backend/src/web/ponepoints/mod.rs
@@ -1,3 +1,5 @@
+use rocket::fairing::AdHoc;
+
 mod points;
 mod pone;
 
@@ -6,10 +8,23 @@ pub use pone::*;
 
 pub struct Client {
     token: String,
+    validation: String,
 }
 
 impl Client {
-    pub fn new(token: String) -> Self {
-        Self { token }
+    pub fn new(token: String, validation: String) -> Self {
+        Self { token, validation }
+    }
+
+    pub fn fairing() -> AdHoc {
+        AdHoc::on_attach("pone points", |rocket| {
+            let cfg = rocket.config();
+            let table = cfg.get_table("ponepoints").unwrap();
+            let token = table["token"].as_str().unwrap().to_string();
+            let validation = table["validation"].as_str().unwrap().to_string();
+            let cli = Self::new(token, validation);
+
+            Ok(rocket.manage(cli))
+        })
     }
 }
